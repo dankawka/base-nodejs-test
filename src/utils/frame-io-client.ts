afbd14ebba0b731459c67a2652b498cccdb76208
Daniel Kawka <daniel.kawka@outlook.com>
2025-02-05 10:05:43 +0100

    Two separate frameIO clients - read and write

--- src/utils/frame-io-client.ts
+++ src/utils/frame-io-client.ts
@@ -1,9 +1,31 @@
-import axios from 'axios'
-import { FRAME_IO_ACCESS_TOKEN } from 'src/config/env-vars'
+import axios from "axios";
+import { Comment } from "src/utils/types";
+import {
+  FRAME_IO_ACCESS_TOKEN,
+  FRAME_IO_READ_TOKEN,
+} from "src/config/env-vars";
 
-export const frameIoClient = axios.create({
-  baseURL: 'https://api.frame.io/v2/',
+export const frameIoWriteClient = axios.create({
+  baseURL: "https://api.frame.io/v2/",
   headers: {
-    'Authorization': `Bearer ${FRAME_IO_ACCESS_TOKEN}`,
+    Authorization: `Bearer ${FRAME_IO_ACCESS_TOKEN}`,
+  },
+});
+
+export const frameIoReadClient = axios.create({
+  baseURL: "https://api.frame.io/v2/",
+  headers: {
+    Authorization: `Bearer ${FRAME_IO_READ_TOKEN}`,
+  },
+});
+
+export const getComment = async (commentId: string) => {
+  try {
+    const response = await frameIoReadClient.get<Comment>(
+      `/comments/${commentId}`
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
   }
-})
+};
