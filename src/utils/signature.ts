526810a782776705c568824e856e5f425073112e
Daniel Kawka <daniel.kawka@outlook.com>
2025-02-05 10:03:43 +0100

    Add signature validation function

---
+++ src/utils/signature.ts
@@ -0,0 +1,40 @@
+import crypto from "crypto";
+import { IncomingHttpHeaders } from "http";
+import { frameIoWebhookHeadersSchema } from "./iconik-custom-action-payload-schema";
+
+// Define types for the payload structure
+interface WebhookPayload {
+  project: { id: string };
+  resource: { id: string; type: string };
+  team: { id: string };
+  type: string;
+  user: { id: string };
+}
+
+export const isSignatureValid = async (
+  headers: IncomingHttpHeaders,
+  secret: string,
+  body: WebhookPayload
+): Promise<boolean> => {
+  const validHeaders = await frameIoWebhookHeadersSchema.validate(headers);
+
+  const signature = validHeaders["x-frameio-signature"];
+  const timestamp = parseInt(validHeaders["x-frameio-request-timestamp"]);
+
+  const currentTimeUTC: number = new Date().getTime();
+  const currentTimestamp: number = currentTimeUTC / 1000;
+  const minutes: number = 5;
+  const expired: boolean = currentTimestamp - timestamp > minutes * 60;
+
+  if (expired) {
+    return false;
+  }
+
+  const stringBody = JSON.stringify(body);
+  const hmac = crypto.createHmac("sha256", secret);
+  const generateSignature: string = hmac
+    .update(`v0:${timestamp}:${stringBody}`)
+    .digest("hex");
+
+  return signature === `v0=${generateSignature}`;
+};
