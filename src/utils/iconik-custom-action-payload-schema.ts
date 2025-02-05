4aa75b41e2086a3360262d17c6967e97b636bd67
Daniel Kawka <daniel.kawka@outlook.com>
2025-02-05 10:04:26 +0100

    Add frame io webhook schema

--- src/utils/iconik-custom-action-payload-schema.ts
+++ src/utils/iconik-custom-action-payload-schema.ts
@@ -1,21 +1,41 @@
-import {
-  array,
-  date,
-  InferType,
-  object,
-  string,
-} from 'yup'
+import { array, date, InferType, object, string } from "yup";
 
 export const iconikCustomActionPayloadSchema = object({
   user_id: string().uuid().required(),
   system_domain_id: string().uuid(),
-  context: string().oneOf(['ASSET']),
+  context: string().oneOf(["ASSET"]),
   action_id: string(),
   asset_ids: array(string().uuid().required()).required(),
   collection_ids: array(string().uuid()),
   saved_search_ids: array(string().uuid()),
   date_created: date().required(),
   auth_token: string().strip(),
-}).json()
+}).json();
 
-export type IconikCustomActionPayload = InferType<typeof iconikCustomActionPayloadSchema>
+export const frameIoWebhookSchema = object({
+  project: object({
+    id: string().uuid().required(),
+  }).required(),
+  resource: object({
+    id: string().uuid().required(),
+    type: string().oneOf(["comment"]).required(),
+  }).required(),
+  team: object({
+    id: string().uuid().required(),
+  }).required(),
+  type: string().oneOf(["comment.created", "comment.deleted"]).required(),
+  user: object({
+    id: string().uuid().required(),
+  }).required(),
+});
+
+export type FrameIoWebhookPayload = InferType<typeof frameIoWebhookSchema>;
+
+export const frameIoWebhookHeadersSchema = object({
+  "x-frameio-signature": string().required(),
+  "x-frameio-request-timestamp": string().required(),
+});
+
+export type IconikCustomActionPayload = InferType<
+  typeof iconikCustomActionPayloadSchema
+>;
