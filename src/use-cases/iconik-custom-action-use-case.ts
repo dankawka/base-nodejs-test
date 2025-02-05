74ff502e41ff3b3cc65478af7e8e3c23fcbd8ba1
Daniel Kawka <daniel.kawka@outlook.com>
2025-02-05 10:05:10 +0100

    Format file

--- src/use-cases/iconik-custom-action-use-case.ts
+++ src/use-cases/iconik-custom-action-use-case.ts
@@ -1,33 +1,45 @@
-import { FRAME_IO_ROOT_ASSET_ID } from 'src/config/env-vars.js'
-import { frameIoClient } from 'src/utils/frame-io-client.js'
-import { iconikClient } from 'src/utils/iconik-client.js'
-import { IconikCustomActionPayload } from 'src/utils/iconik-custom-action-payload-schema.js'
-import { assetCollection } from 'src/utils/mongo-db.js'
+import { FRAME_IO_ROOT_ASSET_ID } from "src/config/env-vars.js";
+import { frameIoWriteClient } from "src/utils/frame-io-client";
+import { iconikClient } from "src/utils/iconik-client.js";
+import { IconikCustomActionPayload } from "src/utils/iconik-custom-action-payload-schema.js";
+import { assetCollection } from "src/utils/mongo-db.js";
 
-export async function iconikCustomActionUseCase(payload: IconikCustomActionPayload) {
-  const assetId = payload.asset_ids[0]
+export async function iconikCustomActionUseCase(
+  payload: IconikCustomActionPayload
+) {
+  const assetId = payload.asset_ids[0];
 
-  const { data: iconikAsset } = await iconikClient.get(`/assets/v1/assets/${assetId}`)
+  const { data: iconikAsset } = await iconikClient.get(
+    `/assets/v1/assets/${assetId}`
+  );
 
   const proxiesResponse = await iconikClient.get(
     `/files/v1/assets/${assetId}/versions/${iconikAsset.versions[0].id}/proxies/`,
-    { params: { per_page: 1, generate_signed_url: true, content_disposition: 'attachment' } },
-  )
+    {
+      params: {
+        per_page: 1,
+        generate_signed_url: true,
+        content_disposition: "attachment",
+      },
+    }
+  );
 
-  const proxy = proxiesResponse.data.objects[0]
+  const proxy = proxiesResponse.data.objects[0];
 
-  const { data: frameIoAsset } = await frameIoClient.post(`/assets/${FRAME_IO_ROOT_ASSET_ID}/children`, {
+  const { data: frameIoAsset } = await frameIoWriteClient.post(
+    `/assets/${FRAME_IO_ROOT_ASSET_ID}/children`,
+    {
       name: iconikAsset.title,
-      type: 'file',
+      type: "file",
       filesize: proxy.size ?? undefined,
       source: {
-        url: proxy.url
-      }
+        url: proxy.url,
+      },
     }
-  )
+  );
 
   await assetCollection.insertOne({
     iconikAssetId: iconikAsset.id,
     frameIoAssetId: frameIoAsset.id,
-  })
+  });
 }
